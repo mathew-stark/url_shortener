7ac4d1f302edbed7e69d9c911286af5a9a1b7361
Matt <hello@gmail.com>
2024-02-14 19:25:31 +0530

    updated config again

--- src/main/java/com/wewoo/in/config/CorsConfig.java
+++ src/main/java/com/wewoo/in/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://*", "https://*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(3600);
